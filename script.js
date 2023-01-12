c1fdbb6ae3cd08b6fab008cb0edb747f4ef67870
kknivves <kqualiotto@gmail.com>
2023-01-11 22:46:50 -0800

    Created calculator class
    Created Calculator class and used a constructor to define the variables for the class. These will allow us a way to set the text elements inside of the calculator class

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+class Calculator {
+  // constructor is a special function that creates and initializes an object instance of a class.
+  constructor(previousOperandandTextElement, currentOperandandTextElement) {
+    // this. in a function it references the object calling that function at a given time.
+    // below are variables for the class. These give us a way to set text elements inside of our calculator class
+    this.previousOperandandTextElement = previousOperandandTextElement;
+    this.currentOperandandTextElement = currentOperandandTextElement;
+    // Next step is to think of all the operations our calculator class can conform (i.e. All Clear function, Delete function, Divide function, etc.)
+  }
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
