3630b37cf2f0df4ef82641992d9f36d465f59549
kknivves <kqualiotto@gmail.com>
2023-01-13 18:02:30 -0800

    Added equalsButton
    Added event listener so that when the user clicks the equals button, the button will run the compute function and the updateDisplay function.

--- script.js
+++ script.js
@@ -72,3 +72,8 @@ operationButtons.forEach((button) => {
     calculator.updateDisplay();
   });
 });
+
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
